0853e2c0db852b2c2ec7639899cb39ff55b1bd5e
ChaseIRL <112039422+vk-chase@users.noreply.github.com>
2023-05-23 13:23:53 -0500

    Update fxmanifest.lua

--- chase-gundealerjob/fxmanifest.lua
+++ chase-gundealerjob/fxmanifest.lua
@@ -1,11 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-is_this_a_map 'yes'
+
 files {'*.*'}
+
 server_script 'server/**.lua'
 client_script 'client/**.lua'
 shared_script 'config.lua'
-
-
-
